a47e3cac717479a059908f1958c7f097d64bed29
shangpin21 <160588683+shangpin21@users.noreply.github.com>
2024-07-21 00:19:49 +0800

    Initial commit

---
+++ src/BasicExercises.java
@@ -0,0 +1,239 @@
+import java.util.Scanner;
+
+public class BasicExercises {
+    public static void main(String[] args) throws Exception {
+        exercise17();
+    }
+
+    // Print the sum of two numbers
+    private static int exercise2(int x, int y){
+        int sum = x + y;
+        return sum;
+    }
+
+    // Q3 divide two numbers 
+    private static double exercise3(int x, int y){
+        double divide = (double) x / y;
+        return divide;
+    }
+
+    /* Q4 Write a Java program to print the results of the following operations.
+    Test Data:
+    a. -5 + 8 * 6
+    b. (55+9) % 9
+    c. 20 + -3*5 / 8
+    d. 5 + 15 / 3 * 2 - 8 % 3
+    */
+
+    private static void exercise4(){
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55 + 9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+
+    // Q5 return product of two numbers
+    private static double exercise5(int x, int y){
+        return x * y;
+    }
+
+    // Q6 return the sum, multiply, subtract, divide and remainder of two numbers
+    private static void exercise6(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input first number: ");
+        int num1 = in.nextInt();
+
+        System.out.println("Input second number: ");
+        int num2 = in.nextInt();
+        
+        // Calculate and print the sum of the two numbers
+        System.out.println(num1 + " + " + num2 +  " = " + (num1 + num2));
+
+        // Calculate and print the difference of the two numbers
+        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+
+        // Calculate and print the multiplication of the two numbers
+        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+
+        // Calculate and print the division of the two numbers
+        System.out.println(num1 + " / " + num2 + " = " + (num1/num2));
+
+        // Calculate and print the remainder of the two numbers
+        System.out.println(num1 + " % " + num2 + " = " + (num1%num2));
+
+    }
+
+    // Q7 return multiplication table of input
+    private static void exercise7(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input number for multiplication table: ");
+        int num = in.nextInt();
+        for(int i = 1; i <= 10; i++){
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+
+    /* Q8
+     Sample Pattern :
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+     */
+
+     private static void exercise8(){
+        System.out.println("   J    a   v     v  a ");
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+     }
+
+     // Q9 compute ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+
+     private static double exercise9(){
+        return ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+     }
+
+     // Q10 compute 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+
+     private static double exercise10(){
+        return 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+     }
+
+     // Q11 print the area of perimeter of a circle
+     private static void exercise11(double r){
+        double area = Math.PI * Math.pow(2, r);
+        double perimeter = Math.PI  * (2 * r);
+
+        System.out.println("Area " + area);
+        System.out.println("Perimeter " + perimeter);
+     }
+
+     // Q12 take 3 numbers as input and calculate the average of the numbers
+     private static double exercise12(int x, int y, int z){
+        double sum = x + y + z;
+        double avg = sum / 3;
+        return avg;
+     }
+
+     // Q13 Calculate and print the area and perimeter of a rectangle
+     private static void exercise13(int x, int y){
+        double area = x * y;
+        double perimeter = 2 * (x + y);
+        System.out.println("Area: " + area + "\nPerimeter: " + perimeter);
+     }
+
+     // Q14 
+     /*
+        * * * * * * ==================================                          
+        * * * * *  ==================================                          
+        * * * * * * ==================================                          
+        * * * * *  ==================================                          
+        * * * * * * ==================================                          
+        * * * * *  ==================================                          
+        * * * * * * ==================================                          
+        * * * * *  ==================================                          
+        * * * * * * ==================================                          
+        ==============================================                          
+        ==============================================                          
+        ==============================================                          
+        ==============================================                          
+        ==============================================                          
+        ==============================================
+      */
+      private static void exercise14(){
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        
+        // Print a row of equal signs to complete the design
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+
+    }
+
+    // Q15 swap two variables
+    private static void exercise15(){
+        String x = "Hello";
+        String y = "World";
+        String temp  = x;
+        x = y;
+        y = temp;
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+
+    // Q17 add two binary numbers
+    private static void exercise17(){
+        // Declare variables to store two binary numbers and the product
+        long binary1, binary2, multiply = 0;
+
+        // Initialize digit and factor variables for processing binary2
+        int digit, factor = 1;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter binary1: ");
+        binary1 = in.nextLong();
+
+        System.out.println("Enter binary2: ");
+        binary2 = in.nextLong();
+
+        // process binary2 to calculate product
+        while (binary2 != 0) {
+            digit = (int)(binary2 % 10);
+            if(digit == 1) {
+                binary1 = binary1 * factor;
+                multiply = binaryproduct((int) binary1, (int) multiply);
+            }else {
+                binary1 = binary1 * factor;
+            }
+            binary2 = binary2 / 10;
+            factor = 10;
+        }
+
+        // Display the product of the two binary numbers
+        System.out.println("Product of two binary numbers " + multiply + "\n");
+
+        
+    }
+    // Method to calculate the product of two binary numbers
+    static int binaryproduct(int binary1, int binary2) {
+    int i = 0, remainder = 0;
+    int[] sum = new int[20];
+    int binary_prod_result = 0;
+
+    while(binary1 != 0 || binary2 != 0){
+        sum[i++] = (binary1 % 10 + binary2 % 10 + remainder) % 2;
+        remainder = (binary1 % 10 + binary2 % 10 + remainder) / 2;
+        binary1 = binary1 / 10;
+        binary2 = binary2 / 10;
+    }
+
+    if(remainder != 0){
+        sum[i++] = remainder;
+    }
+
+    --i;
+
+    while (i >= 0){
+        binary_prod_result = binary_prod_result * 10 + sum[i--];
+    }
+
+    return binary_prod_result;
+ }
+}
